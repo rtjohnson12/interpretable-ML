fd9af736a9dec71981747d8dee5a16e596974042
Ryan Johnson <rtjohnson1206@gmail.com>
2021-09-26 17:32:22 -0700

    :rocket: initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Interpretable Machine Learning with Python by Serg Masis',
+    author='Ryan Johnson',
+    license='',
+)
